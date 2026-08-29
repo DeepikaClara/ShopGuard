04e96b86873b9e319d28306822d3abdbc6e0fcbc
Dpka_rv <97511874+DeepikaClara@users.noreply.github.com>
2026-08-30 00:19:03 +0530

    Remove LDR and update vibration detection logic

--- ShopGuard.ino
+++ ShopGuard.ino
@@ -10,6 +10,16 @@ Skills Acquired → GPIO handling, sensor interfacing, IoT notifications.
 you unknowingly thnkking it has wrong.
 ->Also need to check whether giving 3 V to LDR is working fine since previously i didn't took this sensitive factor 
 ->if stable in both high or low, pir is sensing, but logic has to be pir should sense only when ldr is high and darkness and time should be 11:00 to 5 am. 
+Removed LDR, since already restricted with night timings
+------------------------------------------------------------
+29-08-2026
+PIR HIGH + vibration HIGH → Intrusion confirmed.
+
+PIR LOW + vibration HIGH → Tamper alert (possible break‑in attempt).
+
+PIR HIGH + vibration LOW → Motion alert (someone moving near shop).
+
+Both LOW → Normal state.
 */
 #include <Wire.h>
 #include "RTClib.h"
@@ -18,16 +28,20 @@ RTC_DS3231 rtc;
 
 const uint8_t pir_pin = D6;
 const uint8_t buzzer = D4;
-const uint8_t ldr_pin = D5;
+const uint8_t vib_pin = D5;
 bool previous_pir_state = LOW;
-bool led_reset = 0;
+//bool previous_vib_state = LOW;
+//bool led_reset = 0;
 //bool current_pir_state = LOW;
-unsigned long wait_time_off = 5000;
-unsigned long timer =0;
+unsigned long debouncing_delay = 500;
+unsigned long current_time = 0;
+unsigned long last_trigger =0;
+bool current_vib_state = LOW;
+
 void setup() {
   // put your setup code here, to run once:
   pinMode(pir_pin,INPUT);
-  pinMode(ldr_pin,INPUT);
+  pinMode(vib_pin,INPUT);
   pinMode(buzzer,OUTPUT);
   uint8_t calibration_time = 30;
   Serial.begin(9600);
@@ -35,6 +49,7 @@ void setup() {
   Serial.println("PIR Sensor under calibration...");
 delay(calibration_time * 1000);
 Serial.println("PIR Sensor is ACTIVE!..");
+delay(1000);
 
   if(!rtc.begin())
   {
@@ -56,10 +71,30 @@ bool pir_detection()
 }
 */
 
-bool ldr_state()
+bool vib_state()
   {
-    bool initial_ldr = digitalRead(ldr_pin);
-    return initial_ldr;
+    //current_vib_state = LOW;
+    bool initial_vib = digitalRead(vib_pin);
+     Serial.println("Checking Intrusion...");
+     delay(1000);
+     current_time =millis();
+     Serial.println(current_time);
+     delay(1000);
+            Serial.print("Direct output of vib:");
+    Serial.println(initial_vib);
+      if(current_time - last_trigger > debouncing_delay)
+      {
+          //Serial.println("Stable Intrusion Detected!");
+          //Serial.println("BUZZER IS ON!!!")
+          //digitalWrite(buzzer,HIGH);
+   
+        current_vib_state = initial_vib;
+        last_trigger = current_time;
+      
+        
+      }
+     
+    return current_vib_state ;
   }
 void loop() 
 {
@@ -70,90 +105,56 @@ void loop()
   Serial.print(now.hour());
   Serial.print(":");
   Serial.println(now.minute());
+  delay(1000);
 
   if(now.hour()>=19 || (now.hour()<=5))
   {
     Serial.println("ShopGuard is ACTIVE!!!");
-    uint8_t counters;
-    bool stable = true;
-    bool temp_ldr = LOW;
-    bool current_ldr= ldr_state();
-  
-  //for(start_time=millis();(millis()-start_time)<=5000;counters++)
-    for(counters =1;counters<=5;counters++)
+    bool current_pir_state = digitalRead(pir_pin);
+    bool stable_vib_state = vib_state();
+
+    Serial.print("PIR:");
+    Serial.println(current_pir_state);
+    Serial.print("Vibration:");
+    Serial.println(stable_vib_state);
+    
+    delay(1000);
+    if( (current_pir_state == HIGH) && (stable_vib_state == HIGH) )
     {
-  
+      Serial.println("Motion & Intrusion Detected!");
       delay(1000);
-      temp_ldr = ldr_state();
-      if(temp_ldr == current_ldr)
-      {
-        Serial.print("current_ldr:");
-        Serial.println(current_ldr);
-        Serial.println(("Stability Check, please wait..."));
-        Serial.print("counters:");
-        Serial.println(counters);
-    
-      }
-      else
-      {
-        Serial.print("current_ldr:");
-        Serial.println(current_ldr);
-        Serial.println("Signal is non stable!");
-        Serial.print("counters:");
-        Serial.println(counters);
-        stable = false;
-        break;
-      
-      }
+      digitalWrite(buzzer,HIGH);
+      previous_pir_state = HIGH;
     }
-  
-    if(stable)
+    else if((current_pir_state == HIGH) && (stable_vib_state == LOW))
     {
-      Serial.println("System stable...");
-      bool current_pir_state = digitalRead(pir_pin);
-      Serial.println("current PIR:");
-      Serial.println(current_pir_state);
-      if((previous_pir_state ==LOW ) && (current_pir_state == HIGH))
-      {
-        Serial.println("Motion Detection");
-        Serial.println("Current_ldr:");
-        Serial.println(current_ldr);
-        
-        Serial.println("Stable Darkness.. LED is ON!");
-        digitalWrite(buzzer,HIGH);
-      
-        //digitalWrite(led_pin, HIGH);
-        //Serial.println("LED is ON...");
-        //led_reset = 1;
-      
-        previous_pir_state = HIGH;
-      }
-     
-      else if((previous_pir_state == HIGH ) && (current_pir_state == LOW ) )
-      {
-        Serial.println("Motion Ended");
-        timer = millis();
-        Serial.print("Timer Started at:");
-        Serial.println(timer);
-        previous_pir_state = LOW;
-        led_reset=0;
-      }
-    
-      if((current_pir_state == LOW ) && ((millis() - timer)> wait_time_off) && (led_reset ==0))
-      {
-        Serial.println("No Motion Confirmed..");
-        Serial.println("Stable Brightness.. LED is OFF!");
-        digitalWrite(buzzer,LOW);
-        //digitalWrite(led_pin, LOW);
-        //Serial.println("LED is OFF...");
-        led_reset = 1;
-      
-      }
+      Serial.println("Motion Detected!");
+      delay(1000);
+      //digitalWrite(buzzer,HIGH);
+      previous_pir_state = HIGH;
     }
+    else if((current_pir_state == LOW) && (stable_vib_state == HIGH) )
+    {
+      Serial.println("Vibration without any motion, Be Careful!!!");
+      delay(1000);
+      digitalWrite(buzzer,HIGH);
+      previous_pir_state = LOW;
+    }
+    else if( (current_pir_state == LOW) && (stable_vib_state == LOW))
+    {
+      Serial.println("Things are normal, Dont worry!");
+      delay(1000);
+      digitalWrite(buzzer,LOW);
+      previous_pir_state = LOW;
+    }
+
+    
+
   }
   else
   {
     Serial.println("ShopGuard is INACTIVE :(");
+    delay(1000);
   }
 }
 
